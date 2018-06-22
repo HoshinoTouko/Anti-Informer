967982c1098c3a2211af0dd3b3163cc628ce5d02
Panda <panda@DESKTOP-CETJM5U>
2018-06-22 20:13:42 +0800

    testing result
    the result of testing

--- test/test_safety/test_asymmetric.py
+++ test/test_safety/test_asymmetric.py
@@ -3,8 +3,7 @@ from core.encryption.asymmetric import V1
 import PRG.test_random
 
 if __name__ == "__main__":
-	plaintext = "".join(
-		["This is a plaintext for the test, in order to confirm that this encrypt method is safe."] * 100000)
+	plaintext = "".join(["This is a plaintext for the test, in order to confirm that this encrypt method is safe."] * 100000)
 	private_key, public_key = V1.generate_key()
 	encrypyed_session_key, ciphertext, tag = V1.encrypt(public_key, plaintext)
 	s = [str(bin(byte)) for byte in encrypyed_session_key]
